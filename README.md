dd041c3052d4eff077f362051239c80731ae4f47
Zacatov Andrei <andrei.zacatov@isa.utm.md>
2023-02-07 08:34:08 +0200

    Initial commit.
    Created a project with rebar3. Edited the README file

---
+++ README.md
@@ -0,0 +1,10 @@
+ptr
+=====
+
+An OTP application that holds the tasks provided in the FAF.PTR16.1 course.
+This application consists of 3 projects that span multiple weeks.
+
+Build
+-----
+
+    $ rebar3 compile
---
+++ src/ptr_app.erl
@@ -0,0 +1,18 @@
+%%%-------------------------------------------------------------------
+%% @doc ptr public API
+%% @end
+%%%-------------------------------------------------------------------
+
+-module(ptr_app).
+
+-behaviour(application).
+
+-export([start/2, stop/1]).
+
+start(_StartType, _StartArgs) ->
+    ptr_sup:start_link().
+
+stop(_State) ->
+    ok.
+
+%% internal functions
