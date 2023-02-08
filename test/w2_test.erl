01674728c7c0b9d90a679f651a2f67769546ef80
Zacatov Andrei <andrei.zacatov@isa.utm.md>
2023-02-08 02:05:16 +0200

    Update Tests.

--- test/w2_test.erl
+++ test/w2_test.erl
@@ -1,16 +1,22 @@
 -include_lib("eunit/include/eunit.hrl").
+
 -module(w2_test).
 
-reverse_test() -> []=w2:reverse([]);
-reverse_test() -> [1,2,3] = w2:reverse([3,2,1]);
-reverse_test() -> [1] = w2:reverse([1]).
+reverse_test() ->
+  [] = mylists:reverse([]),
+  [1, 2, 3] = mylists:reverse([3, 2, 1]),
+  [1] = mylists:reverse([1]).
 
+unique_sum_test() ->
+  ?assert(mylists:unique_sum([]) == 0).
 
-unique_sum_test() -> ?assert( w2:unique_sum([]) == 0).
+translator_test() ->
+  Map = #{"mama" => "mother", "papa" => "father"},
+  String = "mama lives with papa",
+  ?assert(w2:translator(Map, String) =:= "mother lives with father").
 
-
-
-translator_test() -> 
-  Map=#{"mama"=>"mother","papa"=>"father"},
-  String="mama lives with papa",
-  ?assert(w2:translator(Map, String)=:="mother lives with father").
+dedup_test() ->
+  [] = mylists:dedup([]),
+  [1] = mylists:dedup([1]),
+  [1, 2, 3] = mylists:dedup([1, 1, 1, 1, 2, 2, 3, 3]),
+  [1, 2, 3, 1] = mylists:dedup([1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 3, 3, 3, 1, 1, 1]).
